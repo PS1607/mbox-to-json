fe5dd97becc0165cb40285d4e7f4e9b86b909c2d
PS1607 <prakharsharma1607@gmail.com>
2023-02-04 19:44:55 +0530

    added progress bar

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import mailbox
 import argparse
 import subprocess
-import os
+from alive_progress import alive_bar
 
 
 def getcharsets(msg):
@@ -12,47 +12,61 @@ def getcharsets(msg):
             charsets.update([c])
     return charsets
 
+
 def getBody(msg):
     while msg.is_multipart():
-        msg=msg.get_payload()[0]
-    t=msg.get_payload(decode=True)
+        msg = msg.get_payload()[0]
+    t = msg.get_payload(decode=True)
     for charset in getcharsets(msg):
-        t=t.decode(charset)
+        t = t.decode(charset)
     return t
 
+
 def main():
-    parser = argparse.ArgumentParser(description = 'Converts MBOX file to JSON')
-    parser.add_argument('filename', help = 'Input MBOX file path')
-    parser.add_argument('-o', '--output', required = False, default = 'file.json',
-                        help = 'Output JSON file path and name. Defaults to same location and name as input file.')
-    parser.add_argument('-a', '--attachments',
-                        action = 'store_true', help = 'Extracts Attachments from the MBOX.')
+    parser = argparse.ArgumentParser(description="Converts MBOX file to JSON")
+    parser.add_argument("filename", help="Input MBOX file path")
+    parser.add_argument(
+        "-o",
+        "--output",
+        required=False,
+        default="file.json",
+        help="Output JSON file path and name. Defaults to same location and name as input file.",
+    )
+    parser.add_argument(
+        "-a",
+        "--attachments",
+        action="store_true",
+        help="Extracts Attachments from the MBOX.",
+    )
 
     args = parser.parse_args()
 
     if args.attachments:
-        #Run extract.py
-        subprocess.run('python src/extract.py -i ' + args.filename, shell = True)
+        # Run extract.py
+        subprocess.run("python src/extract.py -i " + args.filename, shell=True)
 
     MBOX = args.filename
     mbox = mailbox.mbox(MBOX)
+    msg_count = mbox.__len__()
 
     mbox_dict = {}
-    for i, msg in enumerate(mbox):
-        mbox_dict[i] = {}
-        for header in msg.keys():
-            mbox_dict[i][header] = msg[header]
-        try:
-          mbox_dict[i]['Body'] = getBody(msg)
-        except Exception as e:
+    with alive_bar(msg_count) as bar:
+        for i, msg in enumerate(mbox):
+            mbox_dict[i] = {}
+            bar()
+            for header in msg.keys():
+                mbox_dict[i][header] = msg[header]
+            try:
+                mbox_dict[i]["Body"] = getBody(msg)
+            except Exception as e:
+                print("Error Occured at: ", i)
+                print(e)
+            else:
+                continue
 
-          print("Error Occured at: ",i)
-          print(e)
-        else:
-          continue
+    df = pd.DataFrame.from_dict(mbox_dict, orient="index")
+    df.to_json(args.output, orient="records", index="false")
 
-    df = pd.DataFrame.from_dict(mbox_dict, orient='index')
-    df.to_json(args.output, orient = 'records', compression = 'infer', index = 'true')
 
 if __name__ == "__main__":
     # calling the main function
